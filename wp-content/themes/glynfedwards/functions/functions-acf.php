5832a6041402f973839d4a9098c56bbc3d2f198a
rich <rich@nothingatall.net>
2019-10-03 19:34:05 +0100

    bits

--- wp-content/themes/glynfedwards/functions/functions-acf.php
+++ wp-content/themes/glynfedwards/functions/functions-acf.php
@@ -1,6 +1,6 @@
 <?php
 
 	function acfMapsKey() {
-		acf_update_setting('google_api_key', '');
+		acf_update_setting('google_api_key', 'AIzaSyBU_po7B-zsXyGkLaeuTsWmwbd08-pil3A');
 	}
 	add_action('acf/init', 'acfMapsKey');

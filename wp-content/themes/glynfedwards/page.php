368b98b79019227bbbda184367451a63d915dcc4
rich <rich@nothingatall.net>
2019-11-13 21:07:29 +0000

    meh

--- wp-content/themes/glynfedwards/page.php
+++ wp-content/themes/glynfedwards/page.php
@@ -1,7 +1,11 @@
 <?php
 	get_header();
 
-
-
-
+	$this_title = get_the_title();
+?>
+	<div class="content">
+		<div class="article-wrapper">
+			<h2><?php echo $this_title; ?></h2>
+<?php
+	echo '</div>';
 	get_footer();
